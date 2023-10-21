fafc2607de18110067d91c491c4461130b3d4778
prakashkoria <144730098+prakashkoria@users.noreply.github.com>
2023-10-21 15:15:43 +0100

    Add files via upload

---
+++ astrologyMessages.js
@@ -0,0 +1,202 @@
+//create array for the 12 Zodiac signs
+
+const zodiacSigns = [
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+    "Aquarius",
+    "Pisces",
+  ];
+
+  const horoscopeElements = ["Love", "Career", "Health"];
+
+  const astrologyMessages = {
+    Aries: {
+      Love: [
+        "Your love life is about to get more exciting.",
+        "Expect passion and romance today."
+      ],
+      Career: [
+        "You'll have a productive day at work.",
+        "Opportunities for advancement are on the horizon."
+      ],
+      Health: [
+        "It's a good day to focus on your physical well-being.",
+        "Listen to your body and take care of yourself."
+      ]
+    },
+    Taurus: {
+        Love: [
+          "A great day for expressing your love and affection.",
+          "Strengthen your emotional bonds with loved ones."
+        ],
+        Career: [
+          "Your hard work is paying off in your career.",
+          "New opportunities are on the horizon."
+        ],
+        Health: [
+          "Focus on maintaining a healthy lifestyle today.",
+          "Your well-being is a top priority."
+        ]
+      },
+      Gemini: {
+        Love: [
+          "Communication is key in your love life today.",
+          "Engage in deep conversations with your partner."
+        ],
+        Career: [
+          "Adaptability will be your strong suit at work.",
+          "Embrace change and new challenges."
+        ],
+        Health: [
+          "Stay active and keep your mind sharp today.",
+          "Physical and mental health are interconnected."
+        ]
+      },
+      Cancer: {
+        Love: [
+          "Your emotions are in harmony with your partner's.",
+          "Spend quality time with your loved one."
+        ],
+        Career: [
+          "You'll excel in nurturing roles at work.",
+          "Embrace your caring and supportive nature."
+        ],
+        Health: [
+          "Focus on emotional well-being today.",
+          "Nurture your soul and find balance."
+        ]
+      },
+      Leo: {
+        Love: [
+          "You're radiating charisma and attracting love.",
+          "Be open to new romantic adventures."
+        ],
+        Career: [
+          "Your leadership skills shine at work.",
+          "Take charge and lead with confidence."
+        ],
+        Health: [
+          "Indulge in self-care and relaxation today.",
+          "Recharge and pamper yourself."
+        ]
+      },
+      Virgo: {
+        Love: [
+          "Pay attention to the details in your relationship.",
+          "Small gestures of love go a long way."
+        ],
+        Career: [
+          "Organize and streamline your tasks at work.",
+          "Efficiency is your key to success."
+        ],
+        Health: [
+          "Maintain a balanced diet and stay active.",
+          "Healthy habits are your foundation."
+        ]
+      },
+      Libra: {
+        Love: [
+          "Harmony and balance prevail in your love life.",
+          "Embrace the beauty of your relationships."
+        ],
+        Career: [
+          "Diplomacy and cooperation lead to success.",
+          "Find common ground with colleagues."
+        ],
+        Health: [
+          "Prioritize mental and emotional well-being.",
+          "Inner balance is your strength."
+        ]
+      },
+      Scorpio: {
+        Love: [
+          "Intense emotions fuel your love life today.",
+          "Open up and share your deepest feelings."
+        ],
+        Career: [
+          "Your determination leads to career breakthroughs.",
+          "Face challenges with unwavering resolve."
+        ],
+        Health: [
+          "Explore alternative health practices today.",
+          "Rejuvenate your mind and body."
+        ]
+      },
+      Sagittarius: {
+        Love: [
+          "Adventure awaits in your love life.",
+          "Embrace spontaneity and exploration."
+        ],
+        Career: [
+          "Your enthusiasm is infectious at work.",
+          "Inspire and motivate your team."
+        ],
+        Health: [
+          "Engage in outdoor activities for vitality.",
+          "Nature rejuvenates your spirit."
+        ]
+      },
+      Capricorn: {
+        Love: [
+          "Stability and commitment are key in love.",
+          "Build a solid foundation with your partner."
+        ],
+        Career: [
+          "Hard work and discipline yield rewards.",
+          "Set ambitious goals and stay focused."
+        ],
+        Health: [
+          "A structured routine is essential for well-being.",
+          "Balance work and relaxation."
+        ]
+      },
+      Aquarius: {
+        Love: [
+          "Unconventional love approaches lead to excitement.",
+          "Embrace your uniqueness in relationships."
+        ],
+        Career: [
+          "Innovation and creativity shine at work.",
+          "Think outside the box and be bold."
+        ],
+        Health: [
+          "Mental clarity is crucial for overall health.",
+          "Stay true to your intellectual pursuits."
+        ]
+      },
+      Pisces: {
+        Love: [
+          "Connect on a deep emotional level with your partner.",
+          "Intuition guides your romantic choices."
+        ],
+        Career: [
+          "Express your creativity in your work.",
+          "Trust your artistic talents."
+        ],
+        Health: [
+          "Nurture your spiritual well-being today.",
+          "Embrace meditation and relaxation."
+        ]
+      }
+};
+
+function generateAstrologyMessage() {
+    const randomSign = zodiacSigns[Math.floor(Math.random() * zodiacSigns.length)];
+    const randomElement = horoscopeElements[Math.floor(Math.random() * horoscopeElements.length)];
+    const randomMessage = (astrologyMessages[randomSign] && astrologyMessages[randomSign][randomElement]) || ["No data available for this combination."];
+    const randomIndex = Math.floor(Math.random() * randomMessage.length);
+    const message = `${randomSign} ${randomElement} Horoscope: ${randomMessage[randomIndex]}`;
+    return message;
+  }
+
+const randomAstrologyMessage = generateAstrologyMessage();
+console.log(randomAstrologyMessage);
+  
